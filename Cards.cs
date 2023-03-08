077d441087216d6ac2654aef559ce2be2e8089e7
Rxion12 <127319748+Rxion12@users.noreply.github.com>
2023-03-08 21:58:46 +0000

    Add files via upload

---
+++ Cards.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pack deck = new Pack();
+            Console.WriteLine("Which Type of Shuffle would you like?\n [1] Fisher-Yates\n [2] Riffle Shuffle\n [3] No shuffle");
+            string Val = Console.ReadLine();
+            int shuffletype = Convert.ToInt32(Val);
+            Pack.ShuffleCardPack(shuffletype);
+
+
+        }
+    }
+}
