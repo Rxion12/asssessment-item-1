b3c468951e113c0e683b4ae104dcd3c6d505d622
Rxion12 <127319748+Rxion12@users.noreply.github.com>
2023-03-08 22:15:12 +0000

    Add files via upload

---
+++ Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    public enum Value
+    {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13,
+    }
+    public enum Suit
+    {
+        Club = 1,
+        Spade = 2,
+        Heart = 3,
+        Diamond = 4,
+    }
+    public class Card
+    {
+        public Suit Suit { get; set; }
+        public Value Value { get; set; }
+    }
+    
+}
+
---
+++ Pack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    public class Pack
+    {
+        public void Deck()
+        {
+            Reset();
+        }
+
+        public List<Card> Cards { get; set; }
+        public void Reset()
+        {
+            Cards = Enumerable.Range(1, 4)
+                .SelectMany(s => Enumerable.Range(1, 13)
+                                       .Select(c => new Card()
+                                       {
+                                           Suit = (Suit)s,
+                                           Value = (Value)c
+                                       }
+                                              )
+                                       )
+                .ToList();
+        }
+        public static bool ShuffleCardPack(int typeOfShuffle)
+        {
+            if (typeOfShuffle == 1)
+            {
+                return true;
+            }
+            else if (typeOfShuffle == 2)
+            {
+                return true;
+            }
+            else if (typeOfShuffle == 3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            //Shuffles the pack based on the type of shuffle
+        }
+    }
+
+
+
+
+
+    //public static bool shuffleCardPack(int typeOfShuffle)
+    //{
+    //    //Shuffles the pack based on the type of shuffle
+
+    //}
+    //public static Card deal()
+    //{
+    //    //Deals one card
+
+    //}
+    //public static List<Card> dealCard(int amount)
+    //{
+    //    //Deals the number of cards specified by 'amount'
+    //}
+
+}
